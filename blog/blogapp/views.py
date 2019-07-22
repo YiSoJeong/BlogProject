44d25645aaebb8af7d6f0d059a3d5a0b321f35d3
sojeong <soj980312@gmail.com>
2019-07-22 16:41:30 +0900

    Create form to add blogobject

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Blog
 
 def home(request):
@@ -8,3 +9,14 @@ def home(request):
 def detail(request, blog_id):
     details = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'detail.html', {'details': details})
+
+def new(request): # new.html 띄워줌
+    return render(request, 'new.html')
+
+def create(request): #입력받은 내용 > 디비에 넣어줌
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save() # 객체를 디비에 저장 객체.delete는 반대
+    return redirect('/blog/'+str(blog.id))
--- blog/blogproject/urls.py
+++ blog/blogproject/urls.py
@@ -5,5 +5,7 @@ import blogapp.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.home, name="home"),
-    path('blog/<int:blog_id>', blogapp.views.detail, name="detail")
+    path('blog/<int:blog_id>', blogapp.views.detail, name="detail"),
+    path('blog/new/', blogapp.views.new, name="new"),
+    path('blog/create', blogapp.views.create, name="create"),
 ]
