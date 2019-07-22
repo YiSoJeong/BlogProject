04db404569943f3015a9bf1f4fb248155e2fba4d
sojeong <soj980312@gmail.com>
2019-07-22 16:09:59 +0900

    Create and route detail page

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 def home(request):
-    return render(request, 'home.html')
+    blogs = Blog.objects #퀴리셋
+    return render(request, 'home.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    details = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html', {'details': details})
--- blog/blogproject/urls.py
+++ blog/blogproject/urls.py
@@ -4,5 +4,6 @@ import blogapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', blogapp.views.home, name="home")
+    path('', blogapp.views.home, name="home"),
+    path('blog/<int:blog_id>', blogapp.views.detail, name="detail")
 ]
